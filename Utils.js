a93800da42ffe852d6fbdfbaea7eafebfcc319f9
Aliaksei <AliakseiKo@gmail.com>
2020-05-02 15:01:38 +0300

    add jsDoc to Utils.js

--- Utils.js
+++ Utils.js
@@ -1,4 +1,17 @@
-function cacher(func, hashFunc) {
+/**
+ * Converts input parameters to a unique value
+ * @callback hashFunction
+ * @param {...*} arguments - Еhe same parameters as the source function
+ * @returns {*} - unique value for input parameters
+ */
+
+/**
+ * Adds cache feature to the function
+ * @param {Function} func - function which need to add cache feature
+ * @param {hashFunction} hashFunc - function which converts input parameters to a unique value
+ * @returns {Function} - function with cache feature
+ */
+function cacheDecorator(func, hashFunc) {
   const cache = new Map();
   const cachedFunc = function (...args) {
     const hash = hashFunc(...args);
@@ -15,6 +28,13 @@ function cacher(func, hashFunc) {
   return cachedFunc;
 }
 
+/**
+ * Escapes chars in the string
+ * @param {string} str - this string will be escaped
+ * @param {string[]} chars - chars which need escape
+ * @param {boolean} safety - do not escape already escaped chars
+ * @returns {string} - the escaped string
+ */
 function escape(str, chars = [], safety = true) {
   _chars = (chars.join()).replace( new RegExp(`([${escape.regExpStr}])`, "g"), "\\$&" );
   const regExp = (safety) ? `(?<!\\\\)[${_chars}]|\\\\(?![${_chars}]|\\\\)` : `[${_chars}]|\\\\`;
@@ -24,8 +44,8 @@ function escape(str, chars = [], safety = true) {
 escape.regExp = ["^", "$", ".", "*", "+", "?", "(", ")", "[", "]", "{", "}", "|"];
 escape.regExpStr = "\\" + escape.regExp.join("\\") + "\\\\";
 
-escape = cacher(escape, (str, chars = [], safety = true) => {
+escape = cacheDecorator(escape, (str, chars = [], safety = true) => {
   return `[${str}][${chars.join("")}][${safety && safety.toString()}]`;
 });
 
-module.exports = { escape, cacher };
+module.exports = { escape, cacheDecorator };
