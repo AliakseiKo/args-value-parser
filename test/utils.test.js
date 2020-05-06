7d6092c4c128713ecda2f0ca351bec32e5e74435
Aliaksei <AliakseiKo@gmail.com>
2020-05-06 03:35:33 +0300

    refactor util tests

--- test/utils.test.js
+++ test/utils.test.js
@@ -3,88 +3,88 @@ const { escape } = require('../src/utils');
 
 describe('escape function:', () => {
   test('must escape \\ in the string by default', () => {
-    expect(escape('hello\\word')).toEqual('hello\\\\word');
+    expect(escape('hello\\World')).toEqual('hello\\\\World');
     expect(escape('!@"#№$;%^:&?* \\ .,/()[]{}')).toEqual('!@"#№$;%^:&?* \\\\ .,/()[]{}');
   });
 
   test('must escape string safety', () => {
-    expect(escape('hello\\word', [])).toEqual('hello\\word');
-    expect(escape('hello\\word', ['\\'])).toEqual('hello\\\\word');
-    expect(escape('hello\\word', ['\\', '\\'])).toEqual('hello\\\\word');
+    expect(escape('hello\\World', [])).toEqual('hello\\World');
+    expect(escape('hello\\World', ['\\'])).toEqual('hello\\\\World');
+    expect(escape('hello\\World', ['\\', '\\'])).toEqual('hello\\\\World');
 
-    expect(escape('hello\\word')).toEqual('hello\\\\word');
-    expect(escape('hello\\\\word')).toEqual('hello\\\\word');
-    expect(escape('hello\\\\\\word')).toEqual('hello\\\\\\\\word');
+    expect(escape('hello\\World')).toEqual('hello\\\\World');
+    expect(escape('hello\\\\World')).toEqual('hello\\\\World');
+    expect(escape('hello\\\\\\World')).toEqual('hello\\\\\\\\World');
 
     const charSet2 = ['|'];
-    expect(escape('hello|word', charSet2)).toEqual('hello\\|word');
-    expect(escape('hello\\|word', charSet2)).toEqual('hello\\|word');
-    expect(escape('hello\\\\|word', charSet2)).toEqual('hello\\\\|word');
-    expect(escape('hello||word', charSet2)).toEqual('hello\\|\\|word');
-    expect(escape('hello\\||word', charSet2)).toEqual('hello\\|\\|word');
-    expect(escape('hello|\\\\|word', charSet2)).toEqual('hello\\|\\\\|word');
+    expect(escape('hello|World', charSet2)).toEqual('hello\\|World');
+    expect(escape('hello\\|World', charSet2)).toEqual('hello\\|World');
+    expect(escape('hello\\\\|World', charSet2)).toEqual('hello\\\\|World');
+    expect(escape('hello||World', charSet2)).toEqual('hello\\|\\|World');
+    expect(escape('hello\\||World', charSet2)).toEqual('hello\\|\\|World');
+    expect(escape('hello|\\\\|World', charSet2)).toEqual('hello\\|\\\\|World');
 
     const charSet3 = ['|', '\\'];
-    expect(escape('hello|word', charSet3)).toEqual('hello\\|word');
-    expect(escape('hello\\|word', charSet3)).toEqual('hello\\|word');
-    expect(escape('hello\\\\|word', charSet3)).toEqual('hello\\\\\\|word');
-    expect(escape('hello||word', charSet3)).toEqual('hello\\|\\|word');
-    expect(escape('hello\\||word', charSet3)).toEqual('hello\\|\\|word');
-    expect(escape('hello|\\\\|word', charSet3)).toEqual('hello\\|\\\\\\|word');
+    expect(escape('hello|World', charSet3)).toEqual('hello\\|World');
+    expect(escape('hello\\|World', charSet3)).toEqual('hello\\|World');
+    expect(escape('hello\\\\|World', charSet3)).toEqual('hello\\\\\\|World');
+    expect(escape('hello||World', charSet3)).toEqual('hello\\|\\|World');
+    expect(escape('hello\\||World', charSet3)).toEqual('hello\\|\\|World');
+    expect(escape('hello|\\\\|World', charSet3)).toEqual('hello\\|\\\\\\|World');
 
     const charSet4 = ['|', '$'];
-    expect(escape('hello$|word', charSet4)).toEqual('hello\\$\\|word');
-    expect(escape('hello\\$\\|word', charSet4)).toEqual('hello\\$\\|word');
-    expect(escape('hello\\$\\\\|word', charSet4)).toEqual('hello\\$\\\\|word');
-    expect(escape('hello$||word', charSet4)).toEqual('hello\\$\\|\\|word');
-    expect(escape('hello\\$\\||word', charSet4)).toEqual('hello\\$\\|\\|word');
-    expect(escape('hello\\$|\\\\|word', charSet4)).toEqual('hello\\$\\|\\\\|word');
+    expect(escape('hello$|World', charSet4)).toEqual('hello\\$\\|World');
+    expect(escape('hello\\$\\|World', charSet4)).toEqual('hello\\$\\|World');
+    expect(escape('hello\\$\\\\|World', charSet4)).toEqual('hello\\$\\\\|World');
+    expect(escape('hello$||World', charSet4)).toEqual('hello\\$\\|\\|World');
+    expect(escape('hello\\$\\||World', charSet4)).toEqual('hello\\$\\|\\|World');
+    expect(escape('hello\\$|\\\\|World', charSet4)).toEqual('hello\\$\\|\\\\|World');
 
     const charSet5 = ['|', '\\', '^', '$'];
-    expect(escape('^hello|word$', charSet5)).toEqual('\\^hello\\|word\\$');
-    expect(escape('^hello|\\word$', charSet5)).toEqual('\\^hello\\|\\\\word\\$');
-    expect(escape('\\^hello|\\word$\\', charSet5)).toEqual('\\^hello\\|\\\\word\\$\\\\');
-    expect(escape('\\\\^hello\\|\\\\word$\\\\', charSet5)).toEqual('\\\\\\^hello\\|\\\\word\\$\\\\');
+    expect(escape('^hello|World$', charSet5)).toEqual('\\^hello\\|World\\$');
+    expect(escape('^hello|\\World$', charSet5)).toEqual('\\^hello\\|\\\\World\\$');
+    expect(escape('\\^hello|\\World$\\', charSet5)).toEqual('\\^hello\\|\\\\World\\$\\\\');
+    expect(escape('\\\\^hello\\|\\\\World$\\\\', charSet5)).toEqual('\\\\\\^hello\\|\\\\World\\$\\\\');
   });
 
   test('must escape string unsafety', () => {
-    expect(escape('hello\\word', [], false)).toEqual('hello\\word');
-    expect(escape('hello\\word', ['\\'], false)).toEqual('hello\\\\word');
-    expect(escape('hello\\word', ['\\', '\\'], false)).toEqual('hello\\\\word');
+    expect(escape('hello\\World', [], false)).toEqual('hello\\World');
+    expect(escape('hello\\World', ['\\'], false)).toEqual('hello\\\\World');
+    expect(escape('hello\\World', ['\\', '\\'], false)).toEqual('hello\\\\World');
 
     const charSet1 = ['\\'];
-    expect(escape('hello\\word', charSet1, false)).toEqual('hello\\\\word');
-    expect(escape('hello\\\\word', charSet1, false)).toEqual('hello\\\\\\\\word');
-    expect(escape('hello\\\\\\word', charSet1, false)).toEqual('hello\\\\\\\\\\\\word');
+    expect(escape('hello\\World', charSet1, false)).toEqual('hello\\\\World');
+    expect(escape('hello\\\\World', charSet1, false)).toEqual('hello\\\\\\\\World');
+    expect(escape('hello\\\\\\World', charSet1, false)).toEqual('hello\\\\\\\\\\\\World');
 
     const charSet2 = ['|'];
-    expect(escape('hello|word', charSet2, false)).toEqual('hello\\|word');
-    expect(escape('hello\\|word', charSet2, false)).toEqual('hello\\\\|word');
-    expect(escape('hello\\\\|word', charSet2, false)).toEqual('hello\\\\\\|word');
-    expect(escape('hello||word', charSet2, false)).toEqual('hello\\|\\|word');
-    expect(escape('hello\\||word', charSet2, false)).toEqual('hello\\\\|\\|word');
-    expect(escape('hello|\\\\|word', charSet2, false)).toEqual('hello\\|\\\\\\|word');
+    expect(escape('hello|World', charSet2, false)).toEqual('hello\\|World');
+    expect(escape('hello\\|World', charSet2, false)).toEqual('hello\\\\|World');
+    expect(escape('hello\\\\|World', charSet2, false)).toEqual('hello\\\\\\|World');
+    expect(escape('hello||World', charSet2, false)).toEqual('hello\\|\\|World');
+    expect(escape('hello\\||World', charSet2, false)).toEqual('hello\\\\|\\|World');
+    expect(escape('hello|\\\\|World', charSet2, false)).toEqual('hello\\|\\\\\\|World');
 
     const charSet3 = ['|', '\\'];
-    expect(escape('hello|word', charSet3, false)).toEqual('hello\\|word');
-    expect(escape('hello\\|word', charSet3, false)).toEqual('hello\\\\\\|word');
-    expect(escape('hello\\\\|word', charSet3, false)).toEqual('hello\\\\\\\\\\|word');
-    expect(escape('hello||word', charSet3, false)).toEqual('hello\\|\\|word');
-    expect(escape('hello\\||word', charSet3, false)).toEqual('hello\\\\\\|\\|word');
-    expect(escape('hello|\\\\|word', charSet3, false)).toEqual('hello\\|\\\\\\\\\\|word');
+    expect(escape('hello|World', charSet3, false)).toEqual('hello\\|World');
+    expect(escape('hello\\|World', charSet3, false)).toEqual('hello\\\\\\|World');
+    expect(escape('hello\\\\|World', charSet3, false)).toEqual('hello\\\\\\\\\\|World');
+    expect(escape('hello||World', charSet3, false)).toEqual('hello\\|\\|World');
+    expect(escape('hello\\||World', charSet3, false)).toEqual('hello\\\\\\|\\|World');
+    expect(escape('hello|\\\\|World', charSet3, false)).toEqual('hello\\|\\\\\\\\\\|World');
 
     const charSet4 = ['|', '$'];
-    expect(escape('hello$|word', charSet4, false)).toEqual('hello\\$\\|word');
-    expect(escape('hello\\$\\|word', charSet4, false)).toEqual('hello\\\\$\\\\|word');
-    expect(escape('hello\\$\\\\|word', charSet4, false)).toEqual('hello\\\\$\\\\\\|word');
-    expect(escape('hello$||word', charSet4, false)).toEqual('hello\\$\\|\\|word');
-    expect(escape('hello\\$\\||word', charSet4, false)).toEqual('hello\\\\$\\\\|\\|word');
-    expect(escape('hello\\$|\\\\|word', charSet4, false)).toEqual('hello\\\\$\\|\\\\\\|word');
+    expect(escape('hello$|World', charSet4, false)).toEqual('hello\\$\\|World');
+    expect(escape('hello\\$\\|World', charSet4, false)).toEqual('hello\\\\$\\\\|World');
+    expect(escape('hello\\$\\\\|World', charSet4, false)).toEqual('hello\\\\$\\\\\\|World');
+    expect(escape('hello$||World', charSet4, false)).toEqual('hello\\$\\|\\|World');
+    expect(escape('hello\\$\\||World', charSet4, false)).toEqual('hello\\\\$\\\\|\\|World');
+    expect(escape('hello\\$|\\\\|World', charSet4, false)).toEqual('hello\\\\$\\|\\\\\\|World');
 
     const charSet5 = ['|', '\\', '^', '$'];
-    expect(escape('^hello|word$', charSet5, false)).toEqual('\\^hello\\|word\\$');
-    expect(escape('^hello|\\word$', charSet5, false)).toEqual('\\^hello\\|\\\\word\\$');
-    expect(escape('\\^hello|\\word$\\', charSet5, false)).toEqual('\\\\\\^hello\\|\\\\word\\$\\\\');
-    expect(escape('\\\\^hello\\|\\\\word$\\\\', charSet5, false)).toEqual('\\\\\\\\\\^hello\\\\\\|\\\\\\\\word\\$\\\\\\\\');
+    expect(escape('^hello|World$', charSet5, false)).toEqual('\\^hello\\|World\\$');
+    expect(escape('^hello|\\World$', charSet5, false)).toEqual('\\^hello\\|\\\\World\\$');
+    expect(escape('\\^hello|\\World$\\', charSet5, false)).toEqual('\\\\\\^hello\\|\\\\World\\$\\\\');
+    expect(escape('\\\\^hello\\|\\\\World$\\\\', charSet5, false)).toEqual('\\\\\\\\\\^hello\\\\\\|\\\\\\\\World\\$\\\\\\\\');
   });
 });
