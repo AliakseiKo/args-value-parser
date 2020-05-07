4fd63abc894eb3cdf86704f306d17d89dad8cc77
Aliaksei <AliakseiKo@gmail.com>
2020-05-08 01:21:22 +0300

    add one more regexp special character

--- src/utils.js
+++ src/utils.js
@@ -1,15 +1,15 @@
 /**
- * Converts input parameters to a unique value
+ * Converts input parameters to a unique value.
  * @callback hashFunction
- * @param {...*} arguments - Еhe same parameters as the source function
- * @returns {*} - unique value for input parameters
+ * @param {...*} arguments - The same parameters as the source function.
+ * @returns {*} - unique value for input parameters.
  */
 
 /**
- * Adds cache feature to the function
- * @param {Function} func - function which need to add cache feature
- * @param {hashFunction} hashFunc - function which converts input parameters to a unique value
- * @returns {Function} - function with cache feature
+ * Adds cache feature to the function.
+ * @param {Function} func - a function which need to add cache feature.
+ * @param {hashFunction} hashFunc - a function which converts input parameters to a unique value.
+ * @returns {Function} - the input function with cache feature.
  */
 function cacheDecorator(func, hashFunc = (...args) => args.toString()) {
   const cache = new Map();
@@ -26,44 +26,45 @@ function cacheDecorator(func, hashFunc = (...args) => args.toString()) {
     return newResult;
   };
 
+  Object.setPrototypeOf(cachedFunc, func);
   Object.defineProperty(cachedFunc, "name", { value: func.name });
 
   return cachedFunc;
 }
 
 /**
- * Escapes chars in the string
- * @param {string} str - this string will be escaped
- * @param {string[]} chars - chars which need escape
- * @param {boolean} safety - do not escape already escaped chars
- * @returns {string} - the escaped string
+ * Escapes chars in the string.
+ * @param {string} str - this string will be escaped.
+ * @param {Array.<string>} [chars=['\']] - chars which need escape.
+ * @param {boolean} [safety=true] - do not escape already escaped chars.
+ * @returns {string} - the escaped string.
  */
-const escape = cacheDecorator(
-  function (str, chars = ['\\'], safety = true) {
-    return str.replace(
-      new RegExp(`[${
-        chars.join('').replace(new RegExp(`[${escape.regExpStr}]`, 'g'), '\\$&')
-      }]`, 'g'),
-      (match, offset, input) => {
-        if (!safety) return `\\${match}`;
-        const prevChar = input.charAt(offset - 1);
-        if (prevChar === '\\') return match;
-        const nextChar = input.charAt(offset + 1);
-        if (match === '\\') {
-          if (nextChar !== '\\' && chars.includes(nextChar)) return match;
-          let count = 0;
-          let currentChar;
-          while ((++offset, ++count, (currentChar = input.charAt(offset)) === '\\'));
-          if (chars.includes(currentChar)) ++count;
-          if (!(count % 2)) return match;
-        }
-        return `\\${match}`;
+function escape(str, chars = ['\\'], safety = true) {
+  return str.replace(
+    new RegExp(`[${
+      chars.join('').replace(new RegExp(`[${escape.regExpStr}]`, 'g'), '\\$&')
+    }]`, 'g'),
+    (match, offset, input) => {
+      if (!safety) return `\\${match}`;
+      const prevChar = input.charAt(offset - 1);
+      if (prevChar === '\\') return match;
+      const nextChar = input.charAt(offset + 1);
+      if (match === '\\') {
+        if (nextChar !== '\\' && chars.includes(nextChar)) return match;
+        let count = 0;
+        let currentChar;
+        while ((++offset, ++count, (currentChar = input.charAt(offset)) === '\\'));
+        if (chars.includes(currentChar)) ++count;
+        if (!(count % 2)) return match;
       }
-    );
-  }
-);
+      return `\\${match}`;
+    }
+  );
+}
 
-escape.regExp = ['^', '$', '|', '.', '*', '+', '?', '(', ')', '[', ']', '{', '}', "\\"];
+escape.regExp = ['^', '$', '|', '-', '.', '*', '+', '?', '(', ')', '[', ']', '{', '}', "\\"];
 escape.regExpStr = `\\${escape.regExp.join('\\')}`;
 
-module.exports = { escape, cacheDecorator };
+const escapeCached = cacheDecorator(escape);
+
+module.exports = { escape: escapeCached, cacheDecorator };
