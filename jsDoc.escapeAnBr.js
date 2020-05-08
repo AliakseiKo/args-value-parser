f92cae264fc9c2cb3fe0ca3536ac75866f0f0517
Aliaksei <AliakseiKo@gmail.com>
2020-05-08 06:52:50 +0300

    create jsDoc plugin which escape angle brackets

---
+++ jsDoc.escapeAnBr.js
@@ -0,0 +1,18 @@
+function replacer(str) {
+  return str.replace(/<(.*?)>/g, '&lt;$1&gt;');
+}
+
+function descriptionReplace(node) {
+  if (node && node.description) node.description = replacer(node.description);
+}
+
+module.exports = {
+  handlers: {
+    newDoclet({ doclet }) {
+      descriptionReplace(doclet);
+      doclet.params && doclet.params.forEach((param) => descriptionReplace(param));
+      doclet.returns && doclet.returns.forEach((_return) => descriptionReplace(_return));
+      doclet.properties && doclet.properties.forEach((property) => descriptionReplace(property));
+    }
+  }
+};
